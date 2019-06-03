157140e837f1cf588def07c5021c5d18b4e16f8e
Alternity156 <killercode_alternity_@hotmail.com>
2019-06-03 10:14:30 -0300

    Update song_parser.py

--- source/song_parser.py
+++ source/song_parser.py
@@ -35,6 +35,10 @@ class audica_song_parser():
     
     song_ammount = 0
     
+    def reset(self):
+        self.songs = []
+        self.song_ammount = 0
+    
     def audica_path_check(self):
         
         try:
@@ -61,14 +65,33 @@ class audica_song_parser():
     def parse_audica_songs(self):
         
         if self.audica_path_check:
-            for file in audica_songlist:
-                f = audica_location + songs_location + os.sep + file
+            for file in self.audica_songlist:
+                f = self.audica_location + os.sep + self.songs_location + os.sep + file
                 self.parse_song(f)
             
     def parse_song(self, file):
         desc = json.load(ZipFile(file).open("song.desc"))
         
         s = self.song()
+        
+        s.filename = file.split(os.sep)[-1:]
+        
+        try:
+            s.beginner_cues = json.load(ZipFile(file).open("beginner.cues"))
+        except:
+            pass
+        try:
+            s.moderate_cues = json.load(ZipFile(file).open("moderate.cues"))
+        except:
+            pass
+        try:
+            s.advanced_cues = json.load(ZipFile(file).open("advanced.cues"))
+        except:
+            pass
+        try:
+            s.expert_cues = json.load(ZipFile(file).open("expert.cues"))
+        except:
+            pass
         
         s.songID = desc["songID"]
         s.moggSong = desc["moggSong"]
@@ -131,8 +154,13 @@ class audica_song_parser():
         hidden = False
         offset = 0
         author = ""
+        beginner_cues = {}
+        moderate_cues = {}
+        advanced_cues = {}
+        expert_cues = {}
+        filename = ""
     
-parser = audica_song_parser()
-parser.parse_all_songs()
-for song in parser.songs:
-    print song.title + "-" + str(song.tempo) + "bpm"
+#parser = audica_song_parser()
+#parser.parse_all_songs()
+#for song in parser.songs:
+    #print song.title + "-" + str(song.tempo) + "bpm"
